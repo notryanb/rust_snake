2eab4013701ecd84fc01733a22a2385f04bb9a5c
Ryan B <notryanb@gmail.com>
2018-07-19 08:34:26 -0400

    switch from pancurses to termion

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,10 @@
-extern crate pancurses;
+extern crate termion;
 
-use pancurses::*;
+use termion::{color,style};
 
 fn main() {
-    let window = initscr();
-    start_color();
-    use_default_colors();
-    cbreak();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    window.bkgd(COLOR_PAIR(1));
-    let win_w = window.get_max_x();
-    let win_h = window.get_max_y(); 
-
-    window.addstr(&format!("Width {:?}, Height: {:?}\n\n", win_w, win_h));
-
-    window.printw("Type things and press `delete` to quit`\n");
-    window.refresh();
-    window.keypad(true);
-    noecho();
-    loop {
-        match window.getch() {
-            Some(Input::Character(c)) => { window.addch(c); },
-            Some(Input::KeyDC) => break,
-            Some(input) => { window.addstr(&format!("{:?}", input)); },
-            None => ()
-        }
-    }
-    endwin();
+    println!("{}Red", color::Fg(color::Red));
+    println!("{}Blue", color::Fg(color::Blue));
+    println!("{}Blue'n'Bold{}", style::Bold, style::Reset);
+    println!("{}Just plain italic", style::Italic);
 }
