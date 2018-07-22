52b1e7b8a092012b61052e700b770b73a130b605
notryanb <notryanb@gmail.com>
2018-07-22 14:07:39 -0400

    Change grid building to not include boarder

--- src/board.rs
+++ src/board.rs
@@ -1,10 +1,11 @@
+#[derive(PartialEq, Debug)]
 pub enum CellType {
-    Border,
     Food,
     Open,
     Player,
 }
 
+#[derive(PartialEq, Debug)]
 pub struct Cell {
     x: i32,
     y: i32,
@@ -17,35 +18,28 @@ pub struct Board {
     grid: Vec<Cell>,
 }
 
-/// Initializes a board with provided dimensions and builds out the 
-/// physical (written) form
-///
-/// For a board with 1D representation 5x5
-/// [ 
-///     Border, Border, Border, Border, Border,
-///     Border, Open,   Open,   Open,   Border,
-///     Border, Open,   Open,   Open,   Border,
-///     Border, Open,   Open,   Open,   Border,
-///     Border, Border, Border, Border, Border,
-/// ]
-///
-/// Border indicies [0 1 2 3 4 5 9 10 14 15 19 20 21 22 23 24 ]
-/// Playable / Open Indices [ 6 7 8 11 12 13 16 17 18 ]
 impl Board {
     pub fn new(width: i32, height: i32) -> Board {
         Board {
             width,
             height,
-            grid: create_grid(width, height),
+            grid: Board::create_grid(width, height),
         }
     }
 
     fn create_grid(width: i32, height: i32) -> Vec<Cell> {
-        let dimensions = width * height;
-        let mut board_vec: Vec<Cell> = Vec::new();
+        let dimensions = (width * height)as usize;
+        let mut board_vec: Vec<Cell> = Vec::with_capacity(dimensions);
 
         for item in 0..dimensions {
+            let i = item as i32;
+            let cell = Cell {
+                x: i % height,
+                y: i / width,
+                cell_type: CellType::Open,
+            };
 
+            board_vec.push(cell);
         }
 
         board_vec
@@ -58,10 +52,31 @@ mod tests {
 
     #[test]
     fn new_board() {
-        let width = 10;
-        let height = 10;
+        let width = 4;
+        let height = 4;
+        let grid: Vec<Cell> = Vec::with_capacity((width * height) as usize);
         let board = Board::new(width, height);
+        let expected_grid = vec![
+                Cell { x: 0, y: 0, cell_type: CellType::Open },
+                Cell { x: 1, y: 0, cell_type: CellType::Open },
+                Cell { x: 2, y: 0, cell_type: CellType::Open },
+                Cell { x: 3, y: 0, cell_type: CellType::Open },
+                Cell { x: 0, y: 1, cell_type: CellType::Open },
+                Cell { x: 1, y: 1, cell_type: CellType::Open },
+                Cell { x: 2, y: 1, cell_type: CellType::Open },
+                Cell { x: 3, y: 1, cell_type: CellType::Open },
+                Cell { x: 0, y: 2, cell_type: CellType::Open },
+                Cell { x: 1, y: 2, cell_type: CellType::Open },
+                Cell { x: 2, y: 2, cell_type: CellType::Open },
+                Cell { x: 3, y: 2, cell_type: CellType::Open },
+                Cell { x: 0, y: 3, cell_type: CellType::Open },
+                Cell { x: 1, y: 3, cell_type: CellType::Open },
+                Cell { x: 2, y: 3, cell_type: CellType::Open },
+                Cell { x: 3, y: 3, cell_type: CellType::Open },
+            ];
+
         assert_eq!(board.width, width);
         assert_eq!(board.height, height);
+        assert_eq!(board.grid, expected_grid);
     }
 }
